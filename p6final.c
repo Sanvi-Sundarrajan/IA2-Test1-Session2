93fee6b7176ddf3390ca7b558610f02b3facfc0a
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-11 09:28:04 +0000

    p6

---
+++ p6final.c
@@ -0,0 +1,44 @@
+/*Write a program to reverse a string.
+void input_string(char *a);
+int str_reverse(char *a);
+void output(char *a,char reversea);
+input:
+hello
+output:
+olleh*/
+#include <stdio.h>
+void input_string (char *str)
+{
+  printf (" Enter the string \n");
+  scanf ("%s", str);
+}
+int length (char *str)
+{
+  int count = 0;
+  while (str[count] != 0)
+    count++;
+  return count;
+}
+void revstr (char *str1)
+{
+  int i, len, temp;
+  len = length (str1);
+  for (i = 0; i < len / 2; i++)
+    {
+      temp = str1[i];
+      str1[i] = str1[len - i - 1];
+      str1[len - i - 1] = temp;
+    }
+}
+void output (char *str1)
+{
+  printf (" After reversing the string: %s\n", str1);
+}
+int main ()
+{
+  char str[50];
+  input_string (str);
+  revstr (str);
+  output (str);
+  return 0;
+}
