6d36b335f95ae9cb287862dab21fe62c7a1722ac
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-20 05:13:39 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -19,12 +19,6 @@ int input_number()
 }
 int is_composite(int n)
 {
- /*for(int i=2;i<n/2;i++)
- {
-   if(n%i==0)
-    return 1;
- }
- return 0;(Method 2 )*/
  int count=0, f=1;
   while(f<=n)
     {
@@ -50,3 +44,47 @@ int composite= is_composite(n);
 output(n,composite);
 return 0;
 }
+
+/*Write a program find whether a number is a composite number. A Composite number has a factor other than 1 and itself
+int input_number();
+int is_composite(int n);
+void output(int n, int composite);
+input:
+8
+output:
+8 is a composite number.
+
+
+#include<stdio.h>
+#include<math.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+ for(int i=2;i<n/2;i++)
+ {
+   if(n%i==0)
+    return 1;
+ }
+ return 0;
+
+}
+void output(int n, int composite)
+{
+  if(is_composite(n))
+    printf("The number %d is a Composite\n",n);
+  else
+    printf("The number is not Composite\n") ;
+}
+int main ()
+{
+int n=input_number();
+int composite= is_composite(n);
+output(n,composite);
+return 0;
+}*/
